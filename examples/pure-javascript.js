7b7c4a17d0660dde9fc9be5ee2f73f3ea54254ec
executium <45118842+executium@users.noreply.github.com>
2020-07-21 12:54:27 +0700

    Update and rename pure-javascript.js to pure-javascript.html

--- examples/pure-javascript.js
+++
@@ -1,3 +0,0 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/numeral.js/2.0.6/numeral.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
