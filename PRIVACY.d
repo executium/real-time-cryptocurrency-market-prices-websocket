e0d0c75dca8181a168dd96a9f2d541c80ff78d07
executium <45118842+executium@users.noreply.github.com>
2020-08-04 16:10:59 +0700

    Updated

--- PRIVACY.d
+++ PRIVACY.d
@@ -2,7 +2,6 @@
 
 Executium takes your privacy seriously. To better protect your privacy we provide this privacy policy notice explaining the way your personal information is collected and used.
 
-
 ## Collection of Routine Information
 
 This website/app track basic information about their users. This information includes, but is not limited to, IP addresses, browser details, timestamps and referring pages. None of this information can personally identify specific users to this website/app. The information is tracked for routine administration and maintenance purposes.
@@ -11,21 +10,17 @@ This website/app track basic information about their users. This information inc
 
 Where necessary, this app uses cookies to store information about a visitor’s preferences and history in order to better serve the user and/or present theuser with customized content.
 
-
 ## Advertisement and Other Third Parties
 
 Advertising partners and other third parties may use cookies, scripts and/or web beacons to track user activities on this app in order to display advertisements and other useful information. Such tracking is done directly by the third parties through their own servers and is subject to their own privacy policies. This app has no access or control over these cookies, scripts and/or web beacons that may be used by third parties. Learn how to [opt out of Google’s cookie usage](http://www.google.com/privacy_ads.html).
-
 
 ## Links to Third Party Websites
 
 We have included links on this website for your use and reference. We are not responsible for the privacy policies on these websites. You should be aware that the privacy policies of these websites may differ from our own.
 
-
 ## Security
 
 The security of your personal information is important to us, but remember that no method of transmission over the Internet, or method of electronic storage, is 100% secure. While we strive to use commercially acceptable means to protect your personal information, we cannot guarantee its absolute security.
-
 
 ## Changes To This Privacy Policy
 
